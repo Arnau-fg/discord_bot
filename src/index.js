a52ec3db73489441bd18ff4887068be0204c7e13
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-02-12 16:00:21 +0100

    commit rama nueva

--- src/index.js
+++ src/index.js
@@ -6,61 +6,61 @@ const {
   AttachmentBuilder,
 } = require("discord.js");
 
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
-let pitosComidos = 0;
+// let pitosComidos = 0;
 
-fs.readFile("./assets/jsons/penesChupados.json", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  pitos = JSON.parse(data).penes;
-  pitosComidos = pitos;
-});
+// fs.readFile("./assets/jsons/penesChupados.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   pitos = JSON.parse(data).penes;
+//   pitosComidos = pitos;
+// });
 
-const nivelGay = [
-  {
-    name: "Orlito",
-    gayLevel: 1,
-  },
-  {
-    name: "Mangle",
-    gayLevel: 2,
-  },
-  {
-    name: "Adri",
-    gayLevel: 5,
-  },
-  {
-    name: "Felu",
-    gayLevel: 10,
-  },
-  {
-    name: "Nat",
-    gayLevel: 3,
-  },
-  {
-    name: "Cabra",
-    gayLevel: 999999999999,
-  },
-  {
-    name: "Chara",
-    gayLevel: 6,
-  },
-  {
-    name: "Fell",
-    gayLevel: 20,
-  },
-  {
-    name: "Grace",
-    gayLevel: 4,
-  },
-  {
-    name: "Hielo",
-    gayLevel: 7,
-  },
-];
+// const nivelGay = [
+//   {
+//     name: "Orlito",
+//     gayLevel: 1,
+//   },
+//   {
+//     name: "Mangle",
+//     gayLevel: 2,
+//   },
+//   {
+//     name: "Adri",
+//     gayLevel: 5,
+//   },
+//   {
+//     name: "Felu",
+//     gayLevel: 10,
+//   },
+//   {
+//     name: "Nat",
+//     gayLevel: 3,
+//   },
+//   {
+//     name: "Cabra",
+//     gayLevel: 999999999999,
+//   },
+//   {
+//     name: "Chara",
+//     gayLevel: 6,
+//   },
+//   {
+//     name: "Fell",
+//     gayLevel: 20,
+//   },
+//   {
+//     name: "Grace",
+//     gayLevel: 4,
+//   },
+//   {
+//     name: "Hielo",
+//     gayLevel: 7,
+//   },
+// ];
 
 const client = new Client({
   intents: [
@@ -78,25 +78,16 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
   if (message.author.bot) {
     return;
-  } else if (message.content == "cabra") {
-    message.reply("femboy");
-  } else if (message.content == "gloglo") {
-    const attachment = new AttachmentBuilder()
-      .setFile("./assets/gloglo.gif")
-      .setName("gloglo.gif");
-
-    message.channel.send({ files: [attachment] });
-  }
+  } else if (message.content == "hola") {
+    message.reply("Hola, soy un bot");
+   }
 });
 
 client.on("interactionCreate", (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "mas-gay") {
-    const persona1 = interaction.options.get("primera-persona").value;
-    const persona2 = interaction.options.get("segunda-persona").value;
-
-    const masGay = buscarMasGay(persona1, persona2);
+    
 
     interaction.reply(
       `La persona más gay entre ${persona1} y ${persona2} es... ${masGay}!!!!111!1!!!1!11!!`
@@ -104,17 +95,6 @@ client.on("interactionCreate", (interaction) => {
   } else if (interaction.commandName === "gloglo") {
     interaction.reply(`Cabra se ha comido ${++pitosComidos} pene(s)`);
 
-    const content = JSON.stringify({
-      penes: pitosComidos,
-    });
-
-    fs.writeFile("./assets/jsons/penesChupados.json", content, (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // file written successfully
-      }
-    });
   } else if (interaction.commandName === "embed") {
     const embed = new EmbedBuilder()
       .setTitle("Embed title")
@@ -125,18 +105,18 @@ client.on("interactionCreate", (interaction) => {
   }
 });
 
-function buscarMasGay(persona1, persona2) {
-  const gayLevel1 = nivelGay.find((buscar) => {
-    return persona1 == buscar.name;
-  });
+// function buscarMasGay(persona1, persona2) {
+//   const gayLevel1 = nivelGay.find((buscar) => {
+//     return persona1 == buscar.name;
+//   });
 
-  const gayLevel2 = nivelGay.find((buscar) => {
-    return persona2 == buscar.name;
-  });
+//   const gayLevel2 = nivelGay.find((buscar) => {
+//     return persona2 == buscar.name;
+//   });
 
-  return gayLevel1.gayLevel > gayLevel2.gayLevel
-    ? gayLevel1.name
-    : gayLevel2.name;
-}
+//   return gayLevel1.gayLevel > gayLevel2.gayLevel
+//     ? gayLevel1.name
+//     : gayLevel2.name;
+// }
 
 client.login(process.env.TOKEN);
